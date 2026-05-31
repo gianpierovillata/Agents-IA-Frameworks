11701bb26ab1ea4233bb39612ece89369feaac74
Piero <ironpiero1@gmail.com>
2026-05-31 09:56:16 -0300

    mejora de respuesta en stream para parecer un chat bot que responde y no esperar larespuesta completa

--- app.py
+++ app.py
@@ -16,8 +16,21 @@ async def main():
         instructions="""Eres un asistente medico local que responde preguntas sobre medicina general y enfermedades comunes. 
         Quiero que respondas con un lenguaje claro, que desglose la respuesta en pasos y que sea facil de entender para un paciente. y que te bases en los sintomas que el paciente te proporciona.  La respuesta la vas a estructurar en un json con las siguientes claves: 'sintomas', 'diagnostico', 'tratamiento', 'prevencion' """,
     )
+
+
     consulta = input("Ingrese tu pregunta o sintomas del paciente: ")
-    result = await agent.run(consulta)
-    print(result)
+
+    # O con get_final_response() después
+    stream = agent.run(consulta, stream=True)
+    async for update in stream:
+        if update.text:
+            print(update.text, end="", flush=True)
+    final = await stream.get_final_response()
+    print(f"\n\nCompleto: {final.text}")
+
+
 
 asyncio.run(main())
+
+
+# Streaming básico
